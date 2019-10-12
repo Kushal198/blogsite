874136ba309fe7dcf85e818275b9d123506c1ed6
Kushal KC <kushal198@gmail.com>
2019-10-12 21:06:17 +0545

    Added django_heroku

--- mysite/settings.py
+++ mysite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -147,3 +148,5 @@ EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'kushalkc198@gmail.com'
 EMAIL_HOST_PASSWORD = 'kurtcobain'
 DEFAULT_FROM_EMAIL = 'noreply@noreply.com'
+
+django_heroku.settings(locals())
